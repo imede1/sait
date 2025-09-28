7853b517a944b1b2ba8c6b21a404bf228e4e6569
imede1 <coloradodmit@gmail.com>
2025-09-28 23:44:38 +0300

    gol

---
+++ script.js
@@ -0,0 +1,7 @@
+let clickf=0;
+
+const button=document.getElementById('btn');
+button.addEventListener("click", function(){
+    clickf++
+    document.getElementById("count").innerHTML = clickf; 
+})
