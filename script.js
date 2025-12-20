980b5591b257706581d8e4424630321339ba07e9
imede1 <coloradodmit@gmail.com>
2025-12-20 22:52:22 +0300

    redizain

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-let clickf=0;
+let clickf = 0;
 
-const button=document.getElementById('btn');
-button.addEventListener("click", function(){
-    clickf++
-    document.getElementById("count").innerHTML = clickf; 
-})
+const button = document.getElementById('btn');
+const countDisplay = document.getElementById("count");
 
+if(button) {
+    button.addEventListener("click", function(){
+        clickf++;
+        countDisplay.innerHTML = clickf;
+        
+        countDisplay.style.textShadow = "0 0 20px #fff";
+        setTimeout(() => {
+             countDisplay.style.textShadow = "0 0 5px #008f11";
+        }, 100);
+    });
+}
